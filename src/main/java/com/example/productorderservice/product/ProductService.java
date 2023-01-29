ab9ca68bf63927bb771de82c150d434a25de27ce
alisyabob <sjs939883@gmail.com>
2023-01-29 23:22:13 +0900

    feat: 스프링부트 테스트로 전환하기

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -1,12 +1,17 @@
 package com.example.productorderservice.product;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
 class ProductService {
     private final ProductPort productPort;
 
-    ProductService(ProductPort productPort) {
+    ProductService(final ProductPort productPort) {
         this.productPort = productPort;
     }
 
+    @Transactional
     public void addProduct(final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
