4dec98ba483821cfe9fa1a1d14d6b77c701e54ba
alisyabob <sjs939883@gmail.com>
2023-01-31 01:46:41 +0900

    test: 상품 조회 기능 구현하기

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import static com.example.productorderservice.product.ProductSteps.상품등록요청;
+import static com.example.productorderservice.product.ProductSteps.상품등록요청_생성;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ProductApiTest extends ApiTest {
@@ -23,22 +25,4 @@ class ProductApiTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
     }
-
-    private static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/products")
-                .then()
-                .log().all().extract();
-    }
-
-    private static AddProductRequest 상품등록요청_생성() {
-        final String name = "상품명";
-        final int price = 1000;
-        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        return new AddProductRequest(name, price, discountPolicy);
-    }
-
 }
